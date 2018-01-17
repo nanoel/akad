bbdaefe1ab7e1c6def865f2db0f698ea48646c9d
nanoel <u.madhead@gmail.com>
2018-01-17 18:52:26 +0200

    first commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,45 @@
+//=require jquery.min.js
+
+// to inlude bootstrap plugins add "=" sign below
+// require javascripts/bootstrap/*.*
+//=require javascripts/bootstrap.min.js
+
+//=require slick.min.js
+
+
+;(function($){
+
+	$(document).ready(function(){
+		var $slider = $('.ba-slider');
+
+		$slider.slick({
+			autoplay: true,
+			autoplaySpeed: 3000,
+			arrows: false,
+			dots: true,
+			slide: '.ba-slide'
+		});
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
